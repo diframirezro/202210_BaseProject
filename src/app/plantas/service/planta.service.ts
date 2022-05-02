05d4090aeea6469d9a4348d4ece0672e599212e8
Diego Ramirez <df.ramirezr1@uniandes.edu.co>
2022-05-01 21:10:17 -0500

    se configurar enviroment y service

--- src/app/plantas/service/planta.service.ts
+++ src/app/plantas/service/planta.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Planta } from '../modelo/planta';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantaService {
 
-constructor() { }
+  private apiUrl = environment.baseUrl;
 
+  constructor(private http: HttpClient) { }
+
+
+  getAllPlantas(): Observable<Planta[]>{
+    return this.http.get<Planta[]>(this.apiUrl);
+}
 }
